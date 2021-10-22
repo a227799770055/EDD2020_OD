eb47b021b9e147080db7814d85cd941bc8222fa4
eddie.hsiao <eddie.hsiao@mic.com.tw>
2021-10-22 14:12:40 +0800

    add dataset_split function

--- bbox_transfer.py
+++ bbox_transfer.py
@@ -7,28 +7,32 @@ bbox_yolo_path=r'.\EndoCV2020-Endoscopy-Disease-Detection-Segmentation-subChalle
 types=['BE', 'suspicious', 'HGD', 'cancer', 'polyp']
 
 #read file_name 
-bbox_files=os.listdir(path=bbox_path)
-for i in bbox_files:
-    box=[]
-    path=os.path.join(bbox_path, i)
-    with open(path, "r") as f:
-        for line in f.readlines():
-            line = line.split(' ')
-            x_0 = int(line[0])
-            y_0 = int(line[1])
-            x_1 = int(line[2])
-            y_1 = int(line[3])
+def type_transfer():
+    bbox_files=os.listdir(path=bbox_path)
+    for i in bbox_files:
+        box=[]
+        path=os.path.join(bbox_path, i)
+        with open(path, "r") as f:
+            for line in f.readlines():
+                line = line.split(' ')
+                x_0 = int(line[0])
+                y_0 = int(line[1])
+                x_1 = int(line[2])
+                y_1 = int(line[3])
 
-            w=(x_0+x_1)/2
-            h=(y_0+y_1)/2
-            xc=x_0+w
-            yc=y_0+h
-            type = types.index(line[4][:-1])
-            box.append([type, xc, yc, w, h])
-    
-    #另存路徑
-    o_path=os.path.join(bbox_yolo_path, i)
-    with open(o_path, "w") as f:
-        for line in box:
-            f.write(" ".join('%s' %id for id in line)+"\n")
-print("finish convert")
+                w=(x_0+x_1)/2
+                h=(y_0+y_1)/2
+                xc=x_0+w
+                yc=y_0+h
+                type = types.index(line[4][:-1])
+                box.append([type, xc, yc, w, h])
+        
+        #另存路徑
+        o_path=os.path.join(bbox_yolo_path, i)
+        with open(o_path, "w") as f:
+            for line in box:
+                f.write(" ".join('%s' %id for id in line)+"\n")
+    print("finish convert")
+
+if __name__ == '__main__':
+    type_transfer()
---
+++ dataset_split.py
@@ -0,0 +1,29 @@
+import numpy as np
+import os
+
+image_path=r'.\data'
+f=os.listdir(image_path)
+split_ratio=0.8
+
+img_=[]
+#只取 .jpg 後綴的檔案
+for i in f:
+    if os.path.splitext(i)[1] == '.jpg':
+        img_.append(i)
+
+#打亂順序
+np.random.shuffle(img_)
+len_=len(img_)
+len_=int(split_ratio*len_)
+img_train=img_[:len_]
+img_test=img_[len_:]
+
+
+dataset=['img_train.txt', 'img_test.txt']
+data=[img_train, img_test]
+for i, n in zip(dataset, data):
+    p=os.path.join(image_path, i)
+    with open(p, 'w') as f:
+        for line in n:
+            f.write("".join('%s' %id for id in line)+"\n")
+
